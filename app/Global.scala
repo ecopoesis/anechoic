72d7f39d356ab30702833639b23ecb63dd691f03
Mike Roberts <miker@miker.org>
2013-10-14 16:54:54 -0400

    - HSTS added - still tying to fix Procfile for Play 2.2

--- app/Global.scala
+++ app/Global.scala
@@ -16,11 +16,20 @@ object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
   }
 
   override def doFilter(action: EssentialAction): EssentialAction = EssentialAction { request =>
-    if (Play.isProd && request.path.contains(".") && request.path.contains("assets")) {
-      // in prod, cache items with . in their name (images, css, js etc) forever
-      action.apply(request).map(_.withHeaders(
-        CACHE_CONTROL -> "public, max-age=2592000, no-cache"
-      ))
+    if (Play.isProd) {
+      if (request.path.contains(".") && request.path.contains("assets")) {
+        // in prod, cache items with . in their name (images, css, js etc) forever
+        // also add htst
+        action.apply(request).map(_.withHeaders(
+          CACHE_CONTROL -> "public, max-age=2592000, no-cache",
+          "Strict-Transport-Security" -> "max-age=16070400; includeSubDomains"
+        ))
+      } else {
+        // just add hsts to everything else
+        action.apply(request).map(_.withHeaders(
+          "Strict-Transport-Security" -> "max-age=16070400; includeSubDomains"
+        ))
+      }
     } else {
       // don't cache anything else
       action.apply(request).map(_.withHeaders(
